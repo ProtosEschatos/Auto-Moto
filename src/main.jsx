7f9926590ec92d03a3ff8ff264940afb3fd5639e
ProtosEschatos <dario023imsirovic@protonmail.com>
2026-06-29 21:57:14 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import AutoMotoZagreb from './AutoMotoZagreb.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AutoMotoZagreb />
+  </StrictMode>
+)
