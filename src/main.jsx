691b2b243d87dd24e9373b0df3acaaf3e921cf65
ProtosEschatos <dario023imsirovic@protonmail.com>
2026-06-29 22:27:07 +0200

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import AutoMotoZagreb from './AutoMotoZagreb.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AutoMotoZagreb />
-  </StrictMode>
-)
